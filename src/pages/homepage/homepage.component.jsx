035473d500c727ef1c76ef31c4b521b12c147dc9
Michael Williams <theillf8d@outlook.com>
2020-08-09 21:32:38 -0400

    add routing, start setting up routes.

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./homepage.styles.scss";
-import Directory from '../../components/directory/directory.component'
+import Directory from "../../components/directory/directory.component";
 
-const HomePage = () => <div className="homepage">
+const HomePage = () => (
+  <div className="homepage">
     <Directory />
-</div>;
+  </div>
+);
 
 export default HomePage;
